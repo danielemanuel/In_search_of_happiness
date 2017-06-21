3c99ca819aef14e7a00f168c40f7cb234e7a9918
Chris Mitchell <chrisjmit@gmail.com>
2017-06-21 15:35:19 +0100

    imported bootstrapping, banner added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './sun-banner.jpg';
 import './App.css';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+const example = "hello world" // eslint-disable-line
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
           <h2>In Search Of Happiness</h2>
         </div>
       </div>
