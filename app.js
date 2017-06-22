ba6b9a4f16cec7add5fd6cce6d75de4dfc2f94c3
Alex Chalk <alexchalk17@gmail.com>
2017-06-22 16:37:26 -0400

    Add trailing slash to folder in package.json script

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, '../react-ui/build')));
 app.use('/companies', companies);
 
 if (process.env.NODE_ENV === 'production') {
