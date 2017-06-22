6743c19b1b839040ac1b314d839366a9546f3608
Alex Chalk <alexchalk17@gmail.com>
2017-06-22 11:10:39 -0400

    Write first failing feature test

---
+++ client/test/specs/sampleSpec.js
@@ -0,0 +1,36 @@
+var assert = require('assert');
+var chai = require('chai')
+var should = chai.should();
+var expect = chai.expect;
+var fetch = require('node-fetch');
+
+
+describe('webdriver.io page', function() {
+  xit('should have the right title - the fancy generator way', function () {
+    browser.url('http://webdriver.io');
+    var title = browser.getTitle();
+    assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
+  });
+
+  xit('All links should point to an existing page', ()=> {
+    browser.url('http://webdriver.io')
+    let links = browser.elements('a')
+
+    for (let i=0; i < links.value.length; i++) {
+      let link = links.value[i].ELEMENT,
+        href = browser.elementIdAttribute(link,'href'),
+        status = true
+
+      console.log(href.value)
+
+      fetch(href.value)
+        .then( function(res){
+        })
+        .catch( function(err){
+          status = false
+        })
+
+      expect(status).to.be.true;
+    }
+  });
+});
