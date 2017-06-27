a71bcf6b9cfc9854fc59ec80ffc123002ee71c6e
danielemanuel <daniel.costea@vkernel.ro>
2017-06-27 13:56:58 +0100

    Initialise bublle navigator , test and component files.
    Bubble navigator renders without crashing and displays a 'Media' div.

---
+++ react-ui/src/components/bubbleNavigator.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class BubbleNavigator extends Component {
+
+  render() {
+    return (
+      <div id='media'>Media</div>
+    );
+  }
+}
