d81441c447017c31b6f932da53c2e2c665c62fb7
Alex Chalk <alexchalk17@gmail.com>
2017-06-28 06:38:43 -0400

    Closer

--- react-ui/src/components/radar.js
+++ react-ui/src/components/radar.js
@@ -11,20 +11,21 @@ class RadarComparison extends Component {
   componentDidMount() {
     fetch('/api/companies')
       .then(res => res.json())
-      // .then(companies => this.setState({ companies }))
+      .then(companies => this.setState({ companies }))
       .then(companies => this.setState({ data: {
         labels: ['Overall', 'Culture and Values', 'Senior Leadership', 'Compensation and Benefits', 'Career Opportunities', 'Work-life Balance'],
         datasets: [{
           'label': companies[0].name,
           'backgroundColor': 'rgba(153,255,51,0.4)',
           'borderColor': 'rgba(153,255,51,1)',
-          'data': [3, 4, 5, 4, 3, 4]
+          'data': [parseFloat(companies[0].overallRating), 2, 3, 4, 5, 6]
         }]
       }
       }));
   }
 
   render() {
+    console.log(this.state.companies[0]);
     return (
       <div>
         <h2>Radar Example</h2>
