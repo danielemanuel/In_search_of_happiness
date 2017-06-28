adae099aa788373d5568d0661ad87e695c683647
Alex Chalk <alexchalk17@gmail.com>
2017-06-28 12:46:27 -0400

    Initialize karma config for repository

---
+++ react-ui/karma.conf.js
@@ -0,0 +1,79 @@
+// Karma configuration
+// Generated on Mon Jun 12 2017 18:38:06 GMT-0400 (EDT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'fixture'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'lib/*.js',
+      'src/*.js',
+      'test/*Spec.js',
+      '*.html'
+    ],
+    client: {
+      mocha: {
+        // change Karma's debug.html to the mocha web reporter
+        reporter: 'html'
+      }
+    },
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/fixtures/*.html'   : ['html2js'],
+      '*.html'                 : ['html2js'],
+      '**/*.json'              : ['json_fixtures']
+    },
+
+    jsonFixturesPreprocessor: {
+      variableName: '__json__'
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+  });
+};
