ef9b511e0e6488a2b5b493abd83ba0f698c165f6
Alex Chalk <alexchalk17@gmail.com>
2017-06-28 06:22:07 -0400

    interesting reload of console.log on ln:29

--- react-ui/src/components/radar.js
+++ react-ui/src/components/radar.js
@@ -6,37 +6,34 @@ class RadarComparison extends Component {
   state = {companies: [],
     data: {
       labels: ['Overall', 'Culture and Values', 'Senior Leadership', 'Compensation and Benefits', 'Career Opportunities', 'Work-life Balance'],
-
-    }
-  };
-
-  componentWillMount() {
-    fetch('/api/companies')
-      .then(res => res.json())
-      .then(companies => this.setState({ companies }));
-  }
-  // getInitialState() {
-  //   return { showChild: false };
-  // }
-  // _click() {
-  //   this.setState({showChild: !this.state.showChild});
-  // }
-  render() {
-    const company = this.state.companies[0];
-    const data =  {
-      labels: ['Overall', 'Culture and Values', 'Senior Leadership', 'Compensation and Benefits', 'Career Opportunities', 'Work-life Balance'],
-      datasets: [
-        'label': company.name,
+      datasets: [{
+        'label': 'Sploosh',
         'backgroundColor': 'rgba(153,255,51,0.4)',
         'borderColor': 'rgba(153,255,51,1)',
-        'data': [company.overallRating, company.cultureAndValuesRating, company.seniorLeadershipRating, company.compensationAndBenefitsRating, company.careerOpportunitiesRating, company.workLifeBalanceRating]
-      ]
-    };
+        'data': [3, 4, 5, 4, 3, 4]
+      }]
+
+    },
+    company: undefined
+  };
+
+  componentDidMount() {
+    fetch('/api/companies')
+      .then(res => res.json())
+      // .then(companies => this.setState({ companies }))
+      .then(companies => this.setState({ company: companies[0] }));
+  }
+
+  render() {
+    const company = this.state.companies[0];
+    console.log(this.state.company);
+
+
     return (
       <div>
         <h2>Radar Example</h2>
         <button className='compare' onClick={this._click}>Show radar chart comparison</button>
-        <Radar data={ data } />
+        <Radar data={ this.state.data } />
       </div>
     );
   }
