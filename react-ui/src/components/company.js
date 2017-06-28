ad86c0e79be204855d9fe90ea7a3e1a8aa68b33a
Chris Mitchell <chrisjmit@gmail.com>
2017-06-28 14:36:07 +0100

    refactored to remove link ahead of new chart solution

--- react-ui/src/components/company.js
+++ react-ui/src/components/company.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import Table from './table';
 
 class Company extends Component {
   displayName = 'Company';
   render() {
     return (
-      <div id={'CompanyData'}>
-        <Route path="/charts/:companyID" component={Table} />
-        <tr key={this.props.data.companyID}>
-        <td className='logo'><img src={this.props.data.squareLogo} alt={this.props.data.name + ' Logo'}></img></td>
-          <Link id={this.props.data.name} to={'/charts/' + this.props.data.companyID}>
-          <td className='name'>{this.props.data.name}</td>
-          </Link>
-        <td className='rating'>{this.props.data.overallRating}</td>
-        <td className='website'><a href={'http://'+this.props.data.website} target='_blank'>{this.props.data.website}</a></td>
-        <td className='industry'>{this.props.data.industryName}</td>
-        </tr>
-      </div>
+		<tr key={this.props.data.companyID}>
+      <td className='logo'><img src={this.props.data.squareLogo} alt={this.props.data.name + ' Logo'}></img></td>
+      <td className='name'>{this.props.data.name}</td>
+      <td className='rating'>{this.props.data.overallRating}</td>
+      <td className='website'><a href={'http://'+this.props.data.website} target='_blank'>{this.props.data.website}</a></td>
+      <td className='industry'>{this.props.data.industryName}</td>
+  </tr>
     );
   }
 }
