066db5785f383aab6821ff00cc180de0a21d60bc
Alex Chalk <alexchalk17@gmail.com>
2017-06-28 13:58:50 -0400

    Add second passing test and refactor

--- react-ui/src/components/bubbleNavigator.test.js
+++ react-ui/src/components/bubbleNavigator.test.js
@@ -1,22 +1,23 @@
-
-
 describe('BubbleNavigator',function() {
 
+  var bubbleNavigator;
   beforeEach(function(){
     var fixture = '<div id="BubbleNavigator"> <svg width="1000" height="650"><a id="Information Technology" href="/companies/Information Technology"><circle id="Information TechnologyCircle" r="85" cx="433.7387780733562" cy="539.4409970328319" fill="teal"></circle><text x="433.7387780733562" y="539.4409970328319" text-anchor="middle">Information Technology</text></a></svg></div>';
 
     document.body.insertAdjacentHTML(
          'afterbegin',
          fixture);
+    bubbleNavigator = document.getElementById('BubbleNavigator').innerHTML;
   });
 
-  it('displays the BubbleNavigator bar',function() {
-    expect(document.getElementById('BubbleNavigator').innerHTML).to.include('<circle');
+  it('contains svg', function() {
+    expect(bubbleNavigator).to.include('<svg');
   });
 
-  it('displays the bubbles', function() {
-    expect(document.getElementsByTagName('svg')).to.include('<circle');
+  it('contains a circle',function() {
+    expect(bubbleNavigator).to.include('<circle');
   });
+
 });
    // describe('BubbleNavigator', () => {
 //   let bubbleNavigator = null;
